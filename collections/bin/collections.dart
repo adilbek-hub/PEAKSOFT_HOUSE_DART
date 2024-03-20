3d8171d023db09c226e28b272f321121d03fd78f
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2024-03-20 18:54:19 +0600

    collections done

---
+++ collections/bin/collections.dart
@@ -0,0 +1,24 @@
+void main() {
+  // int a = 1;
+  // int b = 2;
+  // int c = 3;
+
+//  Collections and generic
+// List
+  // List<int> listNames = [10,20,30];
+  // print(listNames);
+// Set
+  Set<bool> setName = {
+    true,
+    false,
+  };
+  print(setName);
+//Map
+  // Map mapName = {
+  //   'name': 'Asan',
+  //   'animal': 'Cow',
+  //   'color': 'White',
+  //   '4': 'Байыш'
+  // };
+  // print(mapName['color']);
+}
