dfaca25de88e437780f25e6ff71615974fd11658
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2024-04-08 20:25:16 +0600

    done function

---
+++ functions/bin/functions.dart
@@ -0,0 +1,10 @@
+void main() {
+  person('Asan', 30);
+  person('Usen', 28);
+  person('Taalay', 23);
+  person('Aygul', 28);
+}
+
+void person(String name, int age) {
+  print('PErson name is: $name, age: $age');
+}
